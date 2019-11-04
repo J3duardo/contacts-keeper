988f8cd720a12d63ff3c5204cf3e201bdf377855
J3duardo <jesus_69eduardo@hotmail.com>
2019-11-04 15:08:38 -0400

    Controladores para tomar los contactos y agregar contactos

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,14 +1,50 @@
 const express = require("express");
 const router = express.Router();
+const {check, validationResult} = require("express-validator");
+const auth = require("../middleware/authMiddleware");
+const User = require("../models/User");
+const Contact = require("../models/Contact");
 
 //Tomar data de todos los contactos
-router.get("/", (req, res) => {
-  res.send("Data de todos los contactos")
+router.get("/", auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({user: req.user.id}).sort({date: -1});
+    res.json({contacts})
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({msg: "Error interno del servidor"})
+  }
 });
 
 //Agregar contacto
-router.post("/", (req, res) => {
-  res.send("Agregar contacto")
+router.post("/", [auth, [
+  check("name", "Name is required").not().isEmpty(),
+  check("phone", "Enter a valid phone number").not().isEmpty()
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()})
+  }
+
+  const {name, email, phone, type} = req.body;
+
+  try {
+    const newContact = new Contact({
+      user: req.user.id,
+      name,
+      email,
+      phone,
+      type
+    });
+
+    const contact = await newContact.save();
+
+    res.json({contact});
+
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({msg: "Error interno del servidor"})
+  }
 });
 
 //Editar un contacto
