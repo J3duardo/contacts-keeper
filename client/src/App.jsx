0a25d41982975c4e972033aaf80ddf7c16b57518
J3duardo <jesus_69eduardo@hotmail.com>
2019-11-05 08:55:25 -0400

    Client side: Mostrar los contactos en página Home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Layout/Navbar";
+import Home from "./components/Pages/Home";
+import About from "./components/Pages/About";
+import ContactState from "./context/contact/ContactState";
+
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      Contacts Keeper  
-    </div>
+    <ContactState>
+      <BrowserRouter>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </BrowserRouter>
+    </ContactState>
   );
 }
 
---
+++ client/src/components/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Contacts from "../Contacts/Contacts";
+
+const Home = () => {
+  return (
+    <div className="grid-2">
+      <div>
+        {/* Formulario de contactos */}
+      </div>
+      <div>
+        <Contacts />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
