506675411bca4c5de954936ec4b8820ed5309381
J3duardo <jesus_69eduardo@hotmail.com>
2019-11-05 08:48:42 -0400

    Client side: Página About

---
+++ client/src/components/Pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p className="my-1">Full stack app for manage contacts</p>
+    </div>
+  );
+}
+
+export default About;
