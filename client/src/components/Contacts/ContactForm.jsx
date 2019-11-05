242e10f96f2be54edd0f460ad7d124c6cbb5906c
J3duardo <jesus_69eduardo@hotmail.com>
2019-11-05 09:39:43 -0400

    Client side: Formulario para creación de contactos

---
+++ client/src/components/Contacts/ContactForm.jsx
@@ -0,0 +1,85 @@
+import React, {useState, useContext} from "react";
+import ContactContext from "../../context/contact/contactContext";
+
+const ContactForm = () => {
+  const contactContext = useContext(ContactContext)
+
+  const [contact, setContact] = useState({
+    name: "",
+    email: "", 
+    phone: "",
+    type: "personal"
+  });
+
+  const onChangeHandler = (e) => {
+    setContact({
+      ...contact,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    contactContext.addContact(contact);
+    setContact({
+      name: "",
+      email: "", 
+      phone: "",
+      type: "personal"
+    })
+  }
+
+  const {name, email, phone, type} = contact;
+
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <h2 className="text-primary">Add Contact</h2>
+      <input 
+        type="text"
+        placeholder="Name"
+        name="name"
+        value={name}
+        onChange={onChangeHandler}
+      />
+      <input 
+        type="email"
+        placeholder="Email"
+        name="email"
+        value={email}
+        onChange={onChangeHandler}
+      />
+      <input 
+        type="text"
+        placeholder="Phone"
+        name="phone"
+        value={phone}
+        onChange={onChangeHandler}
+      />
+      <h5>Contact Type</h5>
+      <label htmlFor="personal">Personal</label>
+      <input 
+        type="radio"
+        id="personal"
+        name="type"
+        value="personal"
+        onChange={onChangeHandler}
+        checked={type === "personal"}
+      />
+      {" "}
+      <label htmlFor="professional">Professional</label>
+      <input 
+        type="radio"
+        id="professional"
+        name="type"
+        value="professional"
+        onChange={onChangeHandler}
+        checked={type === "professional"}
+      />
+      <div>
+        <input type="submit" value="Add contact" className="btn btn-primary btn-block" />
+      </div>
+    </form>
+  );
+}
+
+export default ContactForm;
